ead0320d8bf5092d50263468d6769303dfc61283
rjswjd7089 <73235319+rjswjd7089@users.noreply.github.com>
2020-11-15 22:33:31 +0900

    Update PushPrint.java

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -6,6 +6,7 @@ public class PushPrint {
 		System.out.println("sophomore");
 		System.out.println("Wankwang Univ.");
 		System.out.println("Jeonbuk Iksan");
+		System.out.println("fighting!");
 
 	}
 
