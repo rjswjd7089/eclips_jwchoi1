2b206d443dbcb2f578a47c007bba1dc9c3eb2639
Jeongwon Choi <rjswjd7089@naver.com>
2020-11-15 20:41:19 +0900

    jwchoi branch change a code

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Jeong won Choi");
+		System.out.println("컴퓨터소프트웨어 공학계열");
 	}
 }
